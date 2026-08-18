18d99c547bf86835625821a44a0563435cb57cca
misko <misko@cs.toronto.edu>
2026-08-18 02:15:35 +0000

    tests: keep tandem assertions in release builds

--- tests/test_spf_tandem_metadata.c
+++ tests/test_spf_tandem_metadata.c
@@ -1,6 +1,9 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #include "spf-tandem-metadata.h"
 
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
--- tests/test_spf_tandem_session.c
+++ tests/test_spf_tandem_session.c
@@ -1,6 +1,9 @@
 /* SPDX-License-Identifier: LGPL-2.1-or-later */
 #include "spf-tandem-session.h"
 
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
